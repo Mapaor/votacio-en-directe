65569921a9ef00a1912946bd26c588a0d9c7d368
Martí Pardo <57571130+Mapaor4@users.noreply.github.com>
2025-05-07 01:55:13 +0200

    Update votar.js

--- pages/api/votar.js
+++ pages/api/votar.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Only POST allowed' });
   }
 
-  const { nom }  req.body;
+  const { nom } = req.body;
   if (!nom) {
     return res.status(400).json({ error: 'Nom del curt no rebut' });
   }
